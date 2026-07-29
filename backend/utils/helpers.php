647e4d4e6905e577ead4ad87bc09e3899662809e
imnrqzz <imnrqzz@example.com>
2026-07-29 21:52:54 +0800

    Resolve host LAN IP detection on Windows to allow scanning QR codes via phone

--- backend/utils/helpers.php
+++ backend/utils/helpers.php
@@ -6,14 +6,19 @@
  */
 function getServerIp() {
     $ip = '127.0.0.1';
-    if (isset($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR'] !== '127.0.0.1') {
+    if (isset($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR'] !== '127.0.0.1' && $_SERVER['SERVER_ADDR'] !== '::1') {
         $ip = $_SERVER['SERVER_ADDR'];
-    } elseif (php_uname('s') !== 'Windows') {
-        $sock = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-        if ($sock) {
-            @socket_connect($sock, '8.8.8.8', 80);
-            $ip = @socket_getsockname($sock)[0] ?: '127.0.0.1';
-            @socket_close($sock);
+    } else {
+        $hostIp = gethostbyname(gethostname());
+        if ($hostIp !== '127.0.0.1' && $hostIp !== gethostname()) {
+            $ip = $hostIp;
+        } else {
+            $sock = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+            if ($sock) {
+                @socket_connect($sock, '8.8.8.8', 80);
+                $ip = @socket_getsockname($sock)[0] ?: '127.0.0.1';
+                @socket_close($sock);
+            }
         }
     }
     return $ip;
